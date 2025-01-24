995934f46c24538094bd9dcf365314d2ddd40f18
Ashishdev13 <148112886+Ashishdev13@users.noreply.github.com>
2025-01-23 17:41:21 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://eqqqhxrqgnsxomxjwxln.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxcXFoeHJxZ25zeG9teGp3eGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2NjYyMTcsImV4cCI6MjA1MzI0MjIxN30.Zfo1FvtKvO6aFpfA9k9HEEdJ5RTVsK6Cgc5IPyRvbgs';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
